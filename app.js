1dfcaabd860593b3771ef21d79e4ea2a57eb8518
Theison <theisonbecker@gmail.com>
2026-09-21 18:32:10 -0300

    feat: validações, manuseio de ordens, queries, upload de imagem, user signup, user login e JWT signing

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect('mongodb+srv://theisonbecker_db_user:' +
      process.env.MONGO_ATLAS_PW +
      '@node-rest-shop.2xumje8.mongodb.net/?appName=node-rest-shop'
 );
+mongoose.Promise = global.Promise; 
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -32,6 +35,7 @@ app.use((req, res, next) => {
 // Routes which should handle requests
 app.use('/products', productRoutes); 
 app.use('/orders', orderRoutes); 
+app.use('/user', userRoutes); 
 
 app.use((req, res, next) =>{
     const error = new Error('Not found');
